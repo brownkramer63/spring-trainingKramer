8a8cc92a8003eab17d6396b2441536fe6a241a7f
Kramer Brown <brownkramer63@gmail.com>
2022-09-26 14:39:09 -0500

    springthymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -25,6 +27,23 @@ public class MentorController {
 
             return "mentor/mentor-register";
         }
+
+ @PostMapping("/confirm")
+public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+        // how can i access to "mentor" attribute in this method
+
+
+//if we wanted it to just return rempty page : return mentor/mentor-register
+        return "mentor/mentor-confirmation";
+
+}
+
+@GetMapping("/confirm")
+    public String submitForm2(){
+        return "mentor/mentor-confirmation";
+
+    }
+
 
 
     }
