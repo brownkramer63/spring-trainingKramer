7074d6aaf8299453c9ac72ad487c8837d020b483
Kramer Brown <brownkramer63@gmail.com>
2022-10-23 15:16:20 -0500

    SpringORM

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/Repository/EmployeesRepository.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/Repository/EmployeesRepository.java
@@ -3,5 +3,5 @@ package com.cydeo.Repository;
 import com.cydeo.entity.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeesRepository extends JpaRepository<Employees,Long> {
+public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
 }
--- spring-11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,27 +1,33 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.Repository.CarRepoistory;
+import com.cydeo.Repository.DepartmentRepository;
 import com.cydeo.Repository.EmployeesRepository;
 import com.cydeo.entity.Car;
-import com.cydeo.entity.Employee;
+import com.cydeo.entity.Department;
+import com.cydeo.entity.Employees;
 import com.cydeo.enums.Gender;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
     private final CarRepoistory carRepoistory;
-    private final EmployeesRepository employeesRepository;
+private final EmployeesRepository employeesRepository;
+private final DepartmentRepository departmentRepository;
 
-    public DataGenerator(CarRepoistory carRepoistory) {
+    public DataGenerator(CarRepoistory carRepoistory, EmployeesRepository employeesRepository, DepartmentRepository departmentRepository) {
         this.carRepoistory = carRepoistory;
+        this.employeesRepository = employeesRepository;
+        this.departmentRepository = departmentRepository;
     }
 
-    public DataGenerator(EmployeesRepository employeesRepository) {
-        this.employeesRepository = employeesRepository;
-    }
+
 
 
     @Override
@@ -41,11 +47,26 @@ public class DataGenerator implements CommandLineRunner {
         //to use this mehtod we need to inject this class with a constructor
 
 
-        Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20), Gender.FEMALE, 2500);
-        Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26) Gender.F, 2500);
-        Employee e3 = new Employee("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010,05,17), Gender.F, 2500);
-        Employee e4 = new Employee("Avrom", "Rowantree", null, LocalDate.of(2014,03,02), Gender.M, 2500);
-        Employee e5 = new Employee("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003,01,14), Gender.M, 2500);
+        List<Employees> employeeList = new ArrayList<>();
+        List<Department> departmentList = new ArrayList<>();
 
+        Employees e1 = new Employees("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20), Gender.FEMALE, 2500);
+        Employees e2 = new Employees("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26), Gender.FEMALE, 2500);
+        Employees e3 = new Employees("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010,05,17), Gender.FEMALE, 2500);
+        Employees e4 = new Employees("Avrom", "Rowantree", null, LocalDate.of(2014,03,02), Gender.MALE, 2500);
+        Employees e5 = new Employees("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003,01,14), Gender.MALE, 2500);
+
+        Department d1 = new Department("Sports", "Outdoors");
+        Department d2 = new Department("Tools", "Hardware");
+        Department d3 = new Department("Clothing", "Home");
+        Department d4 = new Department("Phones & Tablets", "Electronics");
+        Department d5 = new Department("Computers", "Electronics");
+
+
+        employeeList.addAll(Arrays.asList(e1,e2,e3,e4,e5));
+        departmentList.addAll(Arrays.asList(d1,d2,d3,d4,d5));
+
+        employeesRepository.saveAll(employeeList);
+        departmentRepository.saveAll(departmentList);
     }
 }
