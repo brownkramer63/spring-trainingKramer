38dc882aa6e96ee6b3d95dc6bfb8c6bf1a6ea9b9
Kramer Brown <brownkramer63@gmail.com>
2022-10-23 11:33:32 -0500

    SpringORM

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO EMPLOYEES(id,name)
 VALUES(1,'Mike');
+
+INSERT INTO student(id,first_name,last_name,email)
+VALUES(1,'Mike','smith','mike@Cydeo.com');
+
+INSERT INTO student(id,first_name,last_name,email)
+VALUES(2,'tom','hanks','tom@Cydeo.com');
