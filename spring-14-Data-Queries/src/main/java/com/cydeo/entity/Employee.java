ac6d08091534f240232fd26a374c772845de2e3a
Kramer Brown <brownkramer63@gmail.com>
2022-10-27 10:38:04 -0500

    SpringORM

--- spring-14-Data-Queries/src/main/java/com/cydeo/entity/Employee.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Employee.java
@@ -1,10 +1,10 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Gender;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -16,5 +16,17 @@ public class Employee extends BaseEntity{
     private String lastName;
     private String email;
     private LocalDate hireDate;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+    private Integer salary;
+
+    @ManyToOne
+    @JoinColumn(name="department")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name="region_id")
+    private Region region;
+
 
 }
