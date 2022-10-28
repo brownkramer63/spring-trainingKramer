f63cad1e907cccb25cb178e32a9bbd08ba86dad3
Kramer Brown <brownkramer63@gmail.com>
2022-10-28 10:51:21 -0500

    SpringORM

--- spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -4,6 +4,7 @@ import com.cydeo.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CourseRepository extends JpaRepository<Course,Long> {
 
@@ -15,6 +16,15 @@ public interface CourseRepository extends JpaRepository<Course,Long> {
 
     //check if a course with the supplied name exists. REurn true if exists, false otherwise
     boolean existsByName(String name);
+
+    //returns the count of course for the given category
+    Integer countByCategory(String category);
+
+    //Find all the course that start with the given course name string
+    List<Course> findCourseByNameIsStartingWith(String name);
+
+    //Find all courses by category and returns a stream
+    Stream<Course> streamAllByCategory(String category);
 
 
 }
