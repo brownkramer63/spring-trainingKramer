3e26b4f0a053a85cc5773833b8c98480029ae701
Kramer Brown <brownkramer63@gmail.com>
2022-10-31 14:05:20 -0500

    SpringORM

--- Spring-13-Data-Cinema-Lab/src/main/java/com/cydeo/Repository/MovieRepository.java
+++ Spring-13-Data-Cinema-Lab/src/main/java/com/cydeo/Repository/MovieRepository.java
@@ -2,10 +2,13 @@ package com.cydeo.Repository;
 
 import com.cydeo.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
@@ -13,13 +16,13 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to read a movie with a name
- Movie findByName(String name);
+ Optional<Movie> findByName(String name);
 
     //Write a derived query to list all movies between a range of prices
 List<Movie> findAllByPriceBetween(BigDecimal price, BigDecimal price2);
 
     //Write a derived query to list all movies where duration exists in the specific list of duration
-
+List<Movie> findAllByDurationIn(List<Integer> durations);
 
     //Write a derived query to list all movies with higher than a specific release date
 
@@ -30,7 +33,8 @@ List<Movie> findAllByPriceBetween(BigDecimal price, BigDecimal price2);
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movies between a range of prices
-
+    @Query("select m from Movie m where m.price between ?1 and ?2")
+List<Movie> getmoviesinrange(@Param("price1") BigDecimal price1, @Param("price2") BigDecimal price2);
 
     //Write a JPQL query that returns all movie names
 
