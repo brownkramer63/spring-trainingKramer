bd47bf36518b7934baaf3ecb65f57a29eecf0172
Kramer Brown <brownkramer63@gmail.com>
2022-11-28 11:44:15 -0600

    API

---
+++ spring-17-REST-ConsumeApis/src/main/java/com/cydeo/controller/Consume_RestTemplate.java
@@ -0,0 +1,45 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/cydeo")
+public class Consume_RestTemplate {
+
+    private final RestTemplate restTemplate;
+private final String URI="https://jsonplaceholder.typicode.com/users";
+
+    public Consume_RestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+ @GetMapping
+ public ResponseEntity<User[]> readAllUsers(){
+
+         return restTemplate.getForEntity(URI, User[].class);
+
+ }
+
+ @GetMapping("{id}")
+ public Object readUser(@PathVariable("id") Integer id){
+
+        String URL = URI+ "/id";
+        return restTemplate.getForObject(URL,Object.class,id);
+
+
+ }
+
+
+
+
+
+
+
+
+}
