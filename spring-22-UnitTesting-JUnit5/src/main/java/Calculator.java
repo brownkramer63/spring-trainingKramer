2a4c9c5c9534d3022d6254d450771ca5fbd37212
Kramer Brown <brownkramer63@gmail.com>
2022-12-15 11:55:01 -0600

    unit testing

--- spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ spring-22-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -1,3 +1,11 @@
 public class Calculator {
+    public Calculator(String operator) {
+        this.operator = operator;
+    }
+
+    public String operator="add";
+    public static int add(int num1, int num2){
+        return num1+num2;
+    }
 
 }
---
+++ spring-22-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void add() {
+        System.out.println("Add Method");
+        int actual=Calculator.add(2,3);
+        assertEquals(5,actual,"Test failed.");
+    }
+
+    @Test
+    void testCase1(){
+fail("not implemented");
+    }
+    @Test
+    void testCase2(){
+        System.out.println("test case 2");
+        assertTrue(Calculator);
+    }
+    @Test
+    void testCase3(){
+
+    }
+    @Test
+    void testCase4(){
+
+    }
+
+}
