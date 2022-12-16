608bd7158c850405ccef08a1b70bd7bf55d47d61
Kramer Brown <brownkramer63@gmail.com>
2022-12-16 11:30:09 -0600

    unit testing

--- spring-23-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/ProjectServiceImplTest.java
+++ spring-23-UnitTesting-Mockito/src/test/java/com/cydeo/service/impl/ProjectServiceImplTest.java
@@ -1,11 +1,36 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.entity.Project;
+import com.cydeo.mapper.ProjectMapper;
+import com.cydeo.repository.ProjectRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProjectServiceImplTest {
+
+    @Mock
+    ProjectRepository projectRepository;
+
+    @Mock
+    ProjectMapper projectMapper;
+
+    @InjectMocks
+    ProjectServiceImpl projectService;
+
+    @Test
+    void getByProjectCode_Test(){
+
+        when(projectRepository.findByProjectCode(anyString())).thenReturn(new Project());//this is called stubbing
+
+
+    }
+
 
 }
